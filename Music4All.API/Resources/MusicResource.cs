80314ee3d1b16f4ce93f55deb52df534c2a9e91d
DiegoDeLaFlor <u202017237@upc.edu.pe>
2022-11-08 17:28:25 -0500

    feat(musician): Added Music Resource.

--- Music4All.API/Resources/MusicResource.cs
+++ Music4All.API/Resources/MusicResource.cs
@@ -1,6 +1,14 @@
-﻿namespace Music4All.API.Resources;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music4All.API.Resources;
 
 public class MusicResource
 {
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; }
     
+    [Required]
+    [MaxLength(150)]
+    public string Description { get; set; }
 }
