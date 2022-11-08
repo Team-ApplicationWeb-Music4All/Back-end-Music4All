56443989fab1e22e14c52aa154990389affba5c3
Arru <u202018777@upc.edu.pe>
2022-11-08 17:11:14 -0500

    feat(Musician): Added Implementation Music Domain of Layer Domain.

--- Music4All.Domain/MusicDomain.cs
+++ Music4All.Domain/MusicDomain.cs
@@ -1,6 +1,38 @@
-﻿namespace Music4All.Domain;
+﻿using Music4All.Infraestructure;
+using Music4All.Infraestructure.Models;
 
-public class MusicDomain
+namespace Music4All.Domain;
+
+public class MusicDomain : IMusicDomain
 {
-    
+    private readonly IMusicRepository _musicRepository;
+     
+    public MusicDomain(IMusicRepository musicRepository)
+    {
+        _musicRepository = musicRepository;
+    }
+    public async Task<List<Music>> getAll(string name)
+    {
+        return await _musicRepository.getAll(name);
+    }
+
+    public async Task<Music> getMusicById(int id)
+    {
+        return await _musicRepository.getMusicById(id);
+    }
+
+    public async Task<bool> createMusic(Music music)
+    {
+        return await _musicRepository.create(music);
+    }
+
+    public async Task<bool> updateMusic(int id, Music music)
+    {
+        return await _musicRepository.Update(id,music);
+    }
+
+    public async Task<bool> deleteMusic(int id)
+    {
+        return await _musicRepository.Delete(id);
+    }
 }
