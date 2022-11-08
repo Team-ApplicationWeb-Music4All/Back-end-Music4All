91dcfce40515dfd9db675ef026ed123bc967be44
AlbaCodePe <105762929+AlbaCodePe@users.noreply.github.com>
2022-11-08 17:26:57 -0600

    new user v5

--- Music4All.API/Controllers/UsersController.cs
+++ Music4All.API/Controllers/UsersController.cs
@@ -1,12 +1,5 @@
-﻿using System.Net.Mime;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace Music4All.API.Controllers;
 
-namespace Music4All.API.Controllers;
-
-//[Authorize]
-[Route("api/v1/[controller]")]
-[ApiController]
-[Produces(MediaTypeNames.Application.Json)]
 public class UsersController
 {
     
