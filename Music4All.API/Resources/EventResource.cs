f63b87a0a4510597c13bd291fa32732465aea7d5
J_Arturo <73813395+pepper419@users.noreply.github.com>
2022-11-08 17:41:06 -0500

    feat(contractor): Added Event resource.

--- Music4All.API/Resources/EventResource.cs
+++ Music4All.API/Resources/EventResource.cs
@@ -1,6 +1,16 @@
-﻿namespace Music4All.API.Resources;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class EventResource
+namespace Music4All.API.Resources;
+
+public class EventsResource
 {
-    
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string Description { get; set; }
+
+    public ContractorResource Contractor { get; set; }
 }
