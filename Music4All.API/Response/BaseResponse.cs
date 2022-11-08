13f3b3d50184902ee5431737b8639e7fdd7b7089
DiegoDeLaFlor <u202017237@upc.edu.pe>
2022-11-08 17:30:51 -0500

    feat(users): Added Base Response Controller.

--- Music4All.API/Response/BaseResponse.cs
+++ Music4All.API/Response/BaseResponse.cs
@@ -1,6 +1,8 @@
 ﻿namespace Music4All.API.Response;
 
-public class BaseResponse
+public abstract class BaseResponse<T>
 {
-    
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Body { get; set; }
 }
