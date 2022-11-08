2b670fd30617e2ca662cdd339c197462930c1ea2
AlbaCodePe <105762929+AlbaCodePe@users.noreply.github.com>
2022-11-08 17:25:40 -0600

    new crontractControllersV1

--- Music4All.API/Controllers/ContractorsController.cs
+++ Music4All.API/Controllers/ContractorsController.cs
@@ -1,14 +1,13 @@
 ﻿using System.Net.Mime;
-using System.Security;
 using Microsoft.AspNetCore.Mvc;
+using Music4All.Domain;
 
 namespace Music4All.API.Controllers;
 
-//[Authorize]
-[Route("api/v1/[controller]")]
-[ApiController]
-[Produces(MediaTypeNames.Application.Json)]
+//[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+//[ApiController]
+//[Produces(MediaTypeNames.Application.Json)]
 public class ContractorsController : ControllerBase
 {
-    
+   
 }
