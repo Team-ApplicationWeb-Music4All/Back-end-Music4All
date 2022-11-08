63f2015336fe446ca45d60c543b41140be16e076
Arru <u202018777@upc.edu.pe>
2022-11-08 17:10:23 -0500

    feat(Musician): Added Interface Music Domain in Domain Layer.

--- Music4All.Domain/IMusicDomain.cs
+++ Music4All.Domain/IMusicDomain.cs
@@ -1,6 +1,13 @@
-﻿namespace Music4All.Domain;
+﻿using Music4All.Infraestructure.Models;
 
-public class IMusicDomain
+namespace Music4All.Domain;
+
+public interface IMusicDomain
 {
-    
+
+    Task<List<Music>> getAll(string name);
+    Task<Music> getMusicById(int id);
+    Task<bool> createMusic(Music music);
+    Task<bool> updateMusic(int id, Music music);
+    Task<bool> deleteMusic(int id);
 }
