2a00ec100ba254d95f0f49edc9c6eeddda821e9b
Arru <u202018777@upc.edu.pe>
2022-11-21 22:50:05 -0500

    feat(Contractors): added Implementation contractor Domain.

---
+++ Music4All.Domain/ContractorDomain.cs
@@ -0,0 +1,44 @@
+﻿using Music4All.Infraestructure;
+using Music4All.Infraestructure.Models;
+
+namespace Music4All.Domain;
+
+public class ContractorDomain: IContractorDomain
+{
+    private readonly IContractorRepository _contractorRepository;
+     
+    public ContractorDomain(IContractorRepository contractorRepository)
+    {
+        _contractorRepository = contractorRepository;
+    }
+    public async Task<List<Contractor>> getAll()
+    {
+        
+        return await _contractorRepository.getAll();
+    }
+
+    public async Task<Contractor> getEventById(int id)
+    {
+        return await _contractorRepository.getEventById(id);
+    }
+
+    public async Task<bool> createEvent(Contractor contractor)
+    {
+        
+        contractor.Id = contractor.Id;
+        contractor.Name = contractor.Name;
+        contractor.Description = contractor.Description;
+        contractor.Age = contractor.Age;
+        return await _contractorRepository.create(contractor);
+    }
+
+    public async Task<bool> updateEvent(int id, Contractor contractor)
+    {
+        return await _contractorRepository.Update(id, contractor);
+    }
+
+    public async Task<bool> deleteEvent(int id)
+    {
+        return await _contractorRepository.Delete(id);
+    }
+}
