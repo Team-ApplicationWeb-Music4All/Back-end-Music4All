9a56daef3d471a249426caa74193feebe4a02210
DiegoDeLaFlor <u202017237@upc.edu.pe>
2022-11-21 23:05:35 -0500

    feat(musician): Added musician domain.

---
+++ Music4All.Domain/IMusicianDomain.cs
@@ -0,0 +1,12 @@
+﻿using Music4All.Infraestructure.Models;
+
+namespace Music4All.Domain;
+
+public interface IMusicianDomain
+{
+    Task<List<Musician>> getAll();
+    Task<Musician> getEventById(int id);
+    Task<bool> createEvent(Musician musician);
+    Task<bool> updateEvent(int id, Musician musician);
+    Task<bool> deleteEvent(int id);
+}
---
+++ Music4All.Domain/MusicianDomain.cs
@@ -0,0 +1,45 @@
+﻿using Music4All.Infraestructure;
+using Music4All.Infraestructure.Models;
+
+namespace Music4All.Domain;
+
+public class MusicianDomain : IMusicianDomain
+{
+    private readonly IMusicianRepository _musicianRepository;
+    private IMusicianDomain _musicianDomainImplementation;
+
+    public MusicianDomain(IMusicianRepository musicianRepository)
+    {
+        _musicianRepository = musicianRepository;
+    }
+    public async Task<List<Musician>> getAll()
+    {
+        
+        return await _musicianRepository.getAll();
+    }
+
+    public async Task<Musician> getEventById(int id)
+    {
+        return await _musicianRepository.getEventById(id);
+    }
+
+    public async Task<bool> createEvent(Musician musician)
+    {
+        
+        musician.Id = musician.Id;
+        musician.Name = musician.Name;
+        musician.Description = musician.Description;
+        musician.Age = musician.Age;
+        return await _musicianRepository.create(musician);
+    }
+
+    public async Task<bool> updateEvent(int id, Musician musician)
+    {
+        return await _musicianRepository.Update(id, musician);
+    }
+
+    public async Task<bool> deleteEvent(int id)
+    {
+        return await _musicianRepository.Delete(id);
+    }
+}
