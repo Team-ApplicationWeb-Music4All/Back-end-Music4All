f6bd6af0ff4d4ad2b12b130bc0ab972a828e93b7
Arru <u202018777@upc.edu.pe>
2022-11-21 22:49:34 -0500

    feat(Contractors): added Interface contractor Domain.

---
+++ Music4All.Domain/IContractorDomain.cs
@@ -0,0 +1,12 @@
+﻿using Music4All.Infraestructure.Models;
+
+namespace Music4All.Domain;
+
+public interface IContractorDomain
+{
+    Task<List<Contractor>> getAll();
+    Task<Contractor> getEventById(int id);
+    Task<bool> createEvent(Contractor contractor);
+    Task<bool> updateEvent(int id, Contractor contractor);
+    Task<bool> deleteEvent(int id);
+}
